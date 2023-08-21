deadf60cfccf00f5a8eeca35f727bff6c7a35b12
Perla-dev <yumadev7@gmail.com>
2023-08-21 08:20:33 -0700

    fix main.c

--- main.c
+++ main.c
@@ -31,6 +31,6 @@ int main(void)
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !";
+	printf("String:[%s]\n", "I am a string !");
 	return (0);
 }
