284596b8c006f105274dc44a970e3ade17c429ce
Perla-dev <yumadev7@gmail.com>
2023-08-22 17:40:45 -0700

    update files

--- oo_printf.c
+++ oo_printf.c
@@ -1,15 +1,13 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include <unistd.h>
 
 /**
- * _printf - Custom printf function.
+ * _printf - Prints output according to a format.
  * @format: String with format specifiers.
  * @...: Set of arguments for format.
  * Return: Number of characters printed (bytes).
  */
-
 int _printf(const char *format, ...)
 {
 	va_list vars;
@@ -19,19 +17,28 @@ int _printf(const char *format, ...)
 	va_start(vars, format);
 
 	for (pointer = format; *pointer; ++pointer)
+	{
 		if (*pointer != '%')
 			_putchar(*pointer), ++bytes;
 		else
 		{
 			++pointer;
-		if (*pointer == '\0')
-			break;
-		if (*pointer == '%')
-			_putchar('%'), ++bytes;
-		else if (*pointer == 'd' || *pointer == 'i')
-			bytes += printf("%d", va_arg(vars, int));
+			if (*pointer == '\0')
+				break;
+			if (*pointer == 'c')
+				_putchar(va_arg(vars, int)), ++bytes;
+			else if (*pointer == 's')
+				bytes += printf("%s", va_arg(vars, char *));
+			else if (*pointer == '%')
+				_putchar('%'), ++bytes;
+			else if (*pointer == 'd' || *pointer == 'i')
+				bytes += printf("%d", va_arg(vars, int));
+			else
+				_putchar('%'), _putchar(*pointer), bytes += 2;
 		}
+	}
 
 	va_end(vars);
 	return (bytes);
 }
+
